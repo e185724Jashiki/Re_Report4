bd2fa6782b7d5dddaf46e0caa465912183fd607f
Munega Jashiki <e185724@ie.u-ryukyu.ac.jp>
2019-12-10 12:37:09 +0900

    課題レポート4，ステップ2のCommit.

--- src/main/jp/ac/uryukyu/ie/e185724/Main.java
+++ src/main/jp/ac/uryukyu/ie/e185724/Main.java
@@ -2,7 +2,15 @@ package jp.ac.uryukyu.ie.e185724;
 
 public class Main {
     public static void main (String[] args) {
-        String str = null;
-        System.out.println(str.length());
+
+
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e) {
+            System.out.println("例外: NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
